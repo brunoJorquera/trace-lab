3580fd01ed615b0bc209fe2a981a87c7fe5a7ff2
brunoJorquera <jorquera.b18@gmail.com>
2021-09-28 16:52:03 -0700

    CSS

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path")
 const app = express()
 
 app.use(express.json())
-app.use(express.static('client'))
+app.use('/public',express.static(path.join(__dirname, "..public")))
 
 const Rollbar = require("rollbar")
 
